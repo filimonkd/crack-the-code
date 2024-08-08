6fd6a2dc0e99cc140ac8d82b5c553377c1258c24
= <=>
2024-08-08 15:05:55 +0300

    updated the gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,22 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const handleLogin = () => {
+    window.location.href = `${
+      import.meta.env.VITE_BACKEND_URL
+    }/api/auth/telegram`;
+  };
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p className="text-3xl italic ...">The quick brown fox ...</p>
-      <p className="not-italic ...">The quick brown fox ...</p>
+      <div className="flex justify-center items-center h-screen">
+        <button
+          onClick={handleLogin}
+          className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+        >
+          Login with Telegram
+        </button>
+      </div>
     </>
   );
 }
