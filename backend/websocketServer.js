931a56349d298beade30798cccb16871ef253f8a
= <=>
2024-08-14 13:22:52 +0300

    fix server error

--- backend/websocketServer.js
+++ backend/websocketServer.js
@@ -1,31 +1,34 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8080 });
 
 let games = {};
 
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
     const parsedMessage = JSON.parse(message);
     const { type, gameId, playerId, data } = parsedMessage;
 
-    console.log(`Received message of type ${type} from ${playerId} in game ${gameId}`);
+    console.log(
+      `Received message of type ${type} from ${playerId} in game ${gameId}`
+    );
 
     switch (type) {
-      case 'createGame':
+      case "createGame":
         games[gameId] = { player1: playerId, player2: null, code: null };
         console.log(`Game ${gameId} created by ${playerId}`);
         break;
 
-      case 'joinGame':
+      case "joinGame":
         if (games[gameId]) {
-          games[gameId].player2 = playerId;
-          console.log(`Player ${playerId} joined game ${gameId}`);
-        } else {
-          console.log(`Game ${gameId} does not exist`);
+          if (!games[gameId].player2) {
+            games[gameId].player2 = playerId;
+          } else if (games[gameId].player1 !== playerId) {
+            ws.send(JSON.stringify({ type: "error", message: "Game is full" }));
+          }
         }
         break;
 
-      case 'lockIn':
+      case "lockIn":
         if (games[gameId]) {
           games[gameId].code = data.code;
           console.log(`Player ${playerId} locked in code for game ${gameId}`);
@@ -34,15 +37,24 @@ wss.on('connection', (ws) => {
         }
         break;
 
-      case 'guess':
+      case "guess":
         if (games[gameId]) {
           const feedback = evaluateGuess(data.guess, games[gameId].code);
-          const winner = feedback === 'Perfect match!' ? playerId : null;
+          const winner = feedback === "Perfect match!" ? playerId : null;
 
-          console.log(`Player ${playerId} guessed ${data.guess} in game ${gameId}. Feedback: ${feedback}`);
+          console.log(
+            `Player ${playerId} guessed ${data.guess} in game ${gameId}. Feedback: ${feedback}`
+          );
 
           // Send feedback to both players
-          ws.send(JSON.stringify({ type: 'feedback', gameId, playerId, data: { feedback, winner } }));
+          ws.send(
+            JSON.stringify({
+              type: "feedback",
+              gameId,
+              playerId,
+              data: { feedback, winner },
+            })
+          );
 
           // Notify the opponent if there is a winner
           if (winner) {
@@ -54,17 +66,17 @@ wss.on('connection', (ws) => {
         break;
 
       default:
-        console.log('Unknown message type:', type);
+        console.log("Unknown message type:", type);
     }
   });
 });
 
 function evaluateGuess(guess, code) {
   if (guess === code) {
-    return 'Perfect match!';
+    return "Perfect match!";
   }
   // Add logic to return meaningful feedback here
-  return 'Some feedback message';
+  return "Some feedback message";
 }
 
 module.exports = wss;
