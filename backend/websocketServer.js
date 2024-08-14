0a2509ffadb4fdfd1605304d2e32ff131bac37ef
= <=>
2024-08-14 13:37:49 +0300

    update server

--- backend/websocketServer.js
+++ backend/websocketServer.js
@@ -14,7 +14,7 @@ wss.on("connection", (ws) => {
 
     switch (type) {
       case "createGame":
-        games[gameId] = { player1: playerId, player2: null, code: null };
+        games[gameId] = { player1: playerId, player2: null, code1: null, code2: null };
         console.log(`Game ${gameId} created by ${playerId}`);
         break;
 
@@ -22,31 +22,53 @@ wss.on("connection", (ws) => {
         if (games[gameId]) {
           if (!games[gameId].player2) {
             games[gameId].player2 = playerId;
-          } else if (games[gameId].player1 !== playerId) {
+            console.log(`Player ${playerId} joined game ${gameId} as player2`);
+          } else {
             ws.send(JSON.stringify({ type: "error", message: "Game is full" }));
+            console.log(`Game ${gameId} is full. ${playerId} could not join.`);
           }
+        } else {
+          ws.send(JSON.stringify({ type: "error", message: "Game does not exist" }));
+          console.log(`Game ${gameId} does not exist`);
         }
         break;
 
       case "lockIn":
         if (games[gameId]) {
-          games[gameId].code = data.code;
-          console.log(`Player ${playerId} locked in code for game ${gameId}`);
+          if (games[gameId].player1 === playerId) {
+            games[gameId].code1 = data.code;
+            console.log(`Player 1 (${playerId}) locked in code for game ${gameId}`);
+          } else if (games[gameId].player2 === playerId) {
+            games[gameId].code2 = data.code;
+            console.log(`Player 2 (${playerId}) locked in code for game ${gameId}`);
+          } else {
+            ws.send(JSON.stringify({ type: "error", message: "Player not recognized in this game" }));
+            console.log(`Player ${playerId} is not recognized in game ${gameId}`);
+          }
         } else {
+          ws.send(JSON.stringify({ type: "error", message: "Game does not exist" }));
           console.log(`Game ${gameId} does not exist`);
         }
         break;
 
       case "guess":
         if (games[gameId]) {
-          const feedback = evaluateGuess(data.guess, games[gameId].code);
+          let opponentCode = games[gameId].player1 === playerId ? games[gameId].code2 : games[gameId].code1;
+
+          if (!opponentCode) {
+            ws.send(JSON.stringify({ type: "error", message: "Opponent's code is not locked in yet" }));
+            console.log(`Opponent's code is not locked in for game ${gameId}`);
+            return;
+          }
+
+          const feedback = evaluateGuess(data.guess, opponentCode);
           const winner = feedback === "Perfect match!" ? playerId : null;
 
           console.log(
             `Player ${playerId} guessed ${data.guess} in game ${gameId}. Feedback: ${feedback}`
           );
 
-          // Send feedback to both players
+          // Send feedback to the guessing player
           ws.send(
             JSON.stringify({
               type: "feedback",
@@ -59,8 +81,18 @@ wss.on("connection", (ws) => {
           // Notify the opponent if there is a winner
           if (winner) {
             console.log(`Player ${playerId} won game ${gameId}`);
+            wss.clients.forEach(client => {
+              if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({
+                  type: "result",
+                  gameId,
+                  data: { winner: playerId }
+                }));
+              }
+            });
           }
         } else {
+          ws.send(JSON.stringify({ type: "error", message: "Game does not exist" }));
           console.log(`Game ${gameId} does not exist`);
         }
         break;
@@ -75,7 +107,7 @@ function evaluateGuess(guess, code) {
   if (guess === code) {
     return "Perfect match!";
   }
-  // Add logic to return meaningful feedback here
+  // Add more detailed evaluation logic here
   return "Some feedback message";
 }
 
